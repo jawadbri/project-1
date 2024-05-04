9fb6b785c63d51a65311f8faaad71c16cd6771ee
jawadbri <jawadalberri@gmail.com>
2024-05-04 21:42:26 +0300

    semi functional with pre placed ships

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let mainGame = document.querySelector('#mainGame')
-let sides = document.querySelectorAll('.test')
-let refresh = document.querySelector('.refresh')
+let board = document.querySelector('#mainGame')
+let sides = document.querySelectorAll('.cell')
+let refresh = document.querySelector('#refresh')
 
 // make a fuction to randomize the ships locations here
 // let side = document
@@ -10,17 +10,22 @@ let refresh = document.querySelector('.refresh')
 // make the click event function work proparly by checking the inside of the sides not the mainGame and see if there's a ship or not
 // if there's a ship, make the box change color, if not, remove one life
 
-// make a fuction that adds a new class (randomly)to [i = i + 5] to use the columns and [i = i + 1 ] for rows
+// make a fuction that adds a new class (randomly)to [i = i + 5] to use the columns or (||) [i = i + 1 ] for rows. with the conditon of not going over the size of the box. so no (i === 4 && 9 && 14 && 19 && 24) for rows and (i === 20 && 21 && 22 && 23 && 24)
 sides.forEach((side, index) => {
   side.addEventListener('click', () => {
     if (side.classList.contains('shipClass')) {
-      console.log('you hit a ship')
+      side.style.backgroundColor = 'green'
+    } else {
+      side.style.backgroundColor = 'red'
+      console.log('u missed')
     }
   })
 })
 // add a small box to the side of the board for hints
 // you get 3 hints that showes you a 2x2 with one ship part inside of them
 refresh.addEventListener('click', () => {
-  // change this to make the refresh button chnage the ship placese and refresh the life counter and hints (if we add hints to begin with)
-  sides.forEach((side) => (side.innerText = '???'))
+  // change this to make the refresh button chnage the ship placese and refresh the life counter and hints (if we add hints to begin with) // sides.forEach((side) => (
+  //   // make it add class to divs with the index of [i=i+5] || [i=i+1] 2 times for 1 ship, 3 for another, 4 for the biggest ship
+  // ))
+  // make it rechange the color back to it's original color
 })
